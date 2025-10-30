af29f96d609a9d0f6a713e7ed5ea6c0ace1e9c6e
Andrew Smithies <a.smithies@littlebill.eu>
2025-10-30 15:56:17 +0100

    Challenges 5 : Créer une interface front-end qui va afficher un champs de saisi. L’utilisateur pourra renseigner un nom et récupérer une liste de clients grâce au endpoint créé dans le challenge 2.

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,12 +1,44 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
-function App() {
-  
+type Customer = {
+  first_name: string;
+  last_name: string;
+};
 
+function App() {
+  const [query, setQuery] = useState('');
+  const [customers, setCustomers] = useState<Customer[]>([]);
+
+  const searchCustomers = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/customers/search', {
+        params: { query }
+      });
+      setCustomers(response.data);
+    } catch (error) {
+      console.error('Error searching customers: ', error);
+      
+    }
+  };
   return (
     <>
-
+      <h1>Customer search</h1>
+      <input 
+        type="text"
+        value={query}
+        onChange={ (e) => setQuery(e.target.value)}
+        placeholder='Search customers...' 
+        />
+      <button onClick={searchCustomers}>Search</button>
+      <ul>
+        {customers.map((customer, index) => (
+          <li key={index}>
+            {customer.first_name}. {customer.last_name}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
